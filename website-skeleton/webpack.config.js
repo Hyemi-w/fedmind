8c4bf21f2a0a5f5814de67e08c643eb39a5f8e5d
hyemi-w <elodie.billon1995@outlook.fr>
2019-10-14 15:18:54 +0200

    home controller created

--- website-skeleton/webpack.config.js
+++ website-skeleton/webpack.config.js
@@ -12,8 +12,10 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
+    .setManifestKeyPrefix('build/')
+    // .copyFiles({
+    //     from: './assets/images',
+    // })
     /*
      * ENTRY CONFIG
      *
